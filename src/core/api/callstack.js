5c327c4a107c40b9f22fc9e91691bbef9613f27f
Jeff Schwartz <jefftschwartz@gmail.com>
2016-03-12 15:51:40 -0500

    Removed more .js files from project.

--- src/core/api/callstack.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var CallStack_1 = require("../callstack/CallStack");
-var UniqueNumber_1 = require("../uniquenumber/UniqueNumber");
-exports.callStack = new CallStack_1.CallStack(new UniqueNumber_1.UniqueNumber());
