7a7a76c52c26a2e2f3452d98e1fc7ee35c1efa2e
Jeff Schwartz <jefftschwartz@gmail.com>
2016-04-23 08:37:35 -0400

    Removed reportSuite from reporting api.

--- src/core/reporters/Reporter.ts
+++ src/core/reporters/Reporter.ts
@@ -4,7 +4,6 @@ import {QueueManagerStats} from "../queue/QueueManager";
 export interface Reporter {
     reportBegin: (configOptions: { version: string, uiTestContainerId: string, name: string, hidePassedTests: boolean }) => void;
     reportSummary: (queueManagerStats: QueueManagerStats) => void;
-    reportSuite: () => void;
     reportSpec: (it: IIt) => void;
     reportEnd: () => void;
 }
